d31f2a1695dbae40b2cd723fcce871b2444e2aa6
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-11-05 09:54:07 +0900

    quiz

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,10 +1,32 @@
 window.addEventListener('load', () => {
   console.log("OK!")
-  const question = "今日の日付は？";
-  const answers = ["11月4日","11月5日","11月6日","11月7日"];
-  const correct = "11月4日";
+  const quiz = [
+    {
+      question : "今日の日付は？",
+      answers : ["11月4日","11月5日","11月6日","11月7日"],
+      correct : "11月4日"
+    },
+    {
+      question : "１＋１は？",
+      answers : ["１","２","３","４"],
+      correct : "２"
+    },
+    {
+      question : "誕生日は？",
+      answers : ["11月26日","11月27日","11月28日","11月29日"],
+      correct : "11月29日"
+    }
+  ];
+
+  const quizCount = quiz.length;
+  let quizIndex = 0;
+
+
+  // const question = "今日の日付は？";
+  // const answers = ["11月4日","11月5日","11月6日","11月7日"];
+  // const correct = "11月4日";
   
-  document.getElementById('q').textContent = question;
+  document.getElementById('q').textContent = quiz[quizIndex].question;
   const ans = document.getElementsByClassName('ans');
   // let i = 0;
   // while (i < ans.length){
@@ -13,7 +35,7 @@ window.addEventListener('load', () => {
     // };
     
     for (let i = 0; i < ans.length; i++){
-      ans[i].textContent = answers[i];
+      ans[i].textContent = quiz[quizIndex].answers[i];
       
       ans[i].addEventListener('click', () => {
         if (ans[i].textContent === correct) {
