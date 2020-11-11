c345e124d59b2f7ce4a056da2a5dcf396eb72457
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-11-11 19:38:27 +0900

    Update top.js

--- app/javascript/top.js
+++ app/javascript/top.js
@@ -2,10 +2,8 @@ window.addEventListener('load', () => {
   const open = document.getElementById('open');
   const menu = document.querySelector('.menu')
   open.addEventListener('mouseover',()=>{
-    // menu.classList.add("show");
   })
   open.addEventListener('mouseout',()=>{
-    // menu.classList.remove("show");
   })
 
 
